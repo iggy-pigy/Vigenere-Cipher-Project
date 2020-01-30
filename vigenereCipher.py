0b1ba24d72569b605195f4d981bbfc0816d55760
iggy-pigy <ilgakokorisa@gmail.com>
2020-01-30 18:15:08 +0000

    Added function encrypt

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -5,3 +5,19 @@ key_phrase = input("Enter key phrase: ")
 
 def to_numbers(word):
     return ([string.ascii_lowercase.index(char) for char in word])
+
+
+def encrypt(word1, word2):
+    sums = [a + b for a, b in zip(to_numbers(word1), to_numbers(word2))]
+    new_sums = []
+    for value in sums:
+        if value > 25:
+            value = value-26
+            new_sums.append(value)
+        else:
+            new_sums.append(value)
+
+    return ''.join([string.ascii_lowercase[index] for index in new_sums])
+
+
+print(encrypt(open_text, key_phrase))
