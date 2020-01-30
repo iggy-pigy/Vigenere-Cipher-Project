dc8e79b87c0e7fb0469b597b72a1dba19b5c5e8c
iggy-pigy <ilgakokorisa@gmail.com>
2020-01-30 18:14:25 +0000

    Added function to_numbers

--- vigenereCipher.py
+++ vigenereCipher.py
@@ -1,2 +1,7 @@
+import string
 open_text = input("Enter open text: ")
 key_phrase = input("Enter key phrase: ")
+
+
+def to_numbers(word):
+    return ([string.ascii_lowercase.index(char) for char in word])
